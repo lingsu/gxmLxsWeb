21ab78e9144c4658fa82693ca965c58ebca8e0a2
lingsu <q25a25q@live.com>
2014-07-13 21:25:59 +0800

    小改进1

---
+++ Presentation/Lxs.Admin/Models/Customers/CustomerModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lxs.Admin.Models.Customers
+{
+    public class CustomerModel
+    {
+        [Required(ErrorMessage = "Username")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Email")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public bool Active { get; set; }
+
+
+        //customer roles
+        public string CustomerRoleNames { get; set; }
+        public List<CustomerRoleModel> AvailableCustomerRoles { get; set; }
+        public int[] SelectedCustomerRoleIds { get; set; }
+    }
+}
---
+++ Presentation/Lxs.Web.Framework/Controllers/ParameterBasedOnFormNameAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Lxs.Web.Framework.Controllers
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true)]
+    public class ParameterBasedOnFormNameAttribute:FilterAttribute,IActionFilter
+    {
+        private readonly string _name;
+        private readonly string _actionParameterName;
+        public ParameterBasedOnFormNameAttribute(string name, string actionParameterName)
+        {
+            this._name = name;
+            this._actionParameterName = actionParameterName;
+        }
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var fromValue = filterContext.RequestContext.HttpContext.Request.Form[_name];
+            filterContext.ActionParameters[_actionParameterName] = !string.IsNullOrEmpty(fromValue);
+        }
+    }
+}
